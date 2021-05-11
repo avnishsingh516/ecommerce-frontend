bfca51b05a94275ce3bb49f64cc8a1ee09d49763
AVNISH SINGH <singhavnish516@gmail.com>
2021-05-12 04:33:14 +0530

    build version added

---
+++ build/precache-manifest.4d43256a20289b6190b0995e4945df96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9036eab895f58d2f1bff599b2c9593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1270b206141a1d7eb0d2",
+    "url": "/static/css/main.cf565b08.chunk.css"
+  },
+  {
+    "revision": "3f919ae53e62ccb98169",
+    "url": "/static/js/2.0c7025f8.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.0c7025f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1270b206141a1d7eb0d2",
+    "url": "/static/js/main.84bb051e.chunk.js"
+  },
+  {
+    "revision": "7186bdaf481ff34a8a5c",
+    "url": "/static/js/runtime-main.342bb8df.js"
+  }
+]);
